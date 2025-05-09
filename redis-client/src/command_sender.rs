018b7d52873414cf7e66062fb4e85464c3216a03
Carlos Castillo <47466248+regexPattern@users.noreply.github.com>
2025-05-09 20:37:18 -0300

    fix: reincorporado ejecutor de comandos anterior

--- redis-client/src/command_sender.rs
+++ redis-client/src/command_sender.rs
@@ -1,8 +1,6 @@
 use commands::Command;
 use commands::pub_sub::PubSubCommand;
 
-use crate::client_command;
-
 use resp::BulkString;
 
 pub enum CommandAction {
@@ -17,39 +15,37 @@ pub enum CommandAction {
 
 //y aca la serelizacion de todos los comanodos..
 pub fn parse_and_serialize_command(input: &str) -> CommandAction {
-    todo!()
-    // match client_command::parse_command(input) {
-    //     Some(Command::PubSub(pubsub_cmd)) => {
-    //         use commands::pub_sub::PubSubCommand::*;
-    //         match pubsub_cmd {
-    //             Subscribe(subscribe) => {
-    //                 let chans = subscribe.channels.clone();
-    //
-    //                 //ETSA BUENO MANEJAR TODO COMO BULK STRINGS INLCUSO INTERNAMENTE?
-    //                 // EN EL SERVER LO HICIMOS PERO QSY
-    //
-    //                 let resp_bytes: Vec<u8> =
-    //                     Command::PubSub(PubSubCommand::Subscribe(subscribe)).into();
-    //                 CommandAction::HandleSubscribe(resp_bytes, chans)
-    //             }
-    //             Unsubscribe(unsubscribe) => {
-    //                 let chans = unsubscribe.channels.clone();
-    //                 let resp_bytes: Vec<u8> =
-    //                     Command::PubSub(PubSubCommand::Unsubscribe(unsubscribe)).into();
-    //                 CommandAction::HandleUnsubscribe(resp_bytes, chans)
-    //             }
-    //             // OJO FALTA QUITTTTT
-    //             _ => {
-    //                 let resp_bytes: Vec<u8> = Command::PubSub(pubsub_cmd).into();
-    //                 CommandAction::Send(resp_bytes)
-    //             }
-    //         }
-    //     }
-    //     //storage commandsa get set publish etc...
-    //     Some(cmd) => {
-    //         let resp_bytes: Vec<u8> = cmd.into();
-    //         CommandAction::Send(resp_bytes)
-    //     }
-    //     None => CommandAction::Unknown(input.trim().to_string()),
-    // }
+    match crate::cli::parse_command(input).unwrap() {
+        Command::PubSub(pubsub_cmd) => {
+            use commands::pub_sub::PubSubCommand::*;
+            match pubsub_cmd {
+                Subscribe(subscribe) => {
+                    let chans = subscribe.channels.clone();
+
+                    //ETSA BUENO MANEJAR TODO COMO BULK STRINGS INLCUSO INTERNAMENTE?
+                    // EN EL SERVER LO HICIMOS PERO QSY
+
+                    let resp_bytes: Vec<u8> =
+                        Command::PubSub(PubSubCommand::Subscribe(subscribe)).into();
+                    CommandAction::HandleSubscribe(resp_bytes, chans)
+                }
+                Unsubscribe(unsubscribe) => {
+                    let chans = unsubscribe.channels.clone();
+                    let resp_bytes: Vec<u8> =
+                        Command::PubSub(PubSubCommand::Unsubscribe(unsubscribe)).into();
+                    CommandAction::HandleUnsubscribe(resp_bytes, chans)
+                }
+                // OJO FALTA QUITTTTT
+                _ => {
+                    let resp_bytes: Vec<u8> = Command::PubSub(pubsub_cmd).into();
+                    CommandAction::Send(resp_bytes)
+                }
+            }
+        }
+        //storage commandsa get set publish etc...
+        cmd => {
+            let resp_bytes: Vec<u8> = cmd.into();
+            CommandAction::Send(resp_bytes)
+        }
+    }
 }
