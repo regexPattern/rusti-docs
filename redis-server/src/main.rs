38f3b6f39f20430413f84cba7673d3b19ff741ce
Carlos Castillo <47466248+regexPattern@users.noreply.github.com>
2025-05-15 18:54:40 -0300

    refactor: reorganizado el main para mejor error handling

--- redis-server/src/main.rs
+++ redis-server/src/main.rs
@@ -1,29 +1,38 @@
+use std::{error, path::Path};
+
 use redis_server::{Config, Server, cli};
 
 fn main() {
     let args = cli::parse();
 
     let config = match args.config_path {
-        Some(path) => {
-            let src = match std::fs::read_to_string(path) {
-                Ok(src) => src,
-                Err(err) => {
-                    eprintln!("ERROR error leyendo archivo de configuración: {err}");
-                    return;
-                }
-            };
-
-            match Config::from_config_file(&src) {
-                Ok(config) => config,
-                Err(err) => {
-                    eprintln!("ERROR error parseando archivo de configuración: {err}");
-                    return;
-                }
+        Some(path) => match read_config_file(&path) {
+            Ok(config) => config,
+            Err(err) => {
+                eprint!(
+                    "{}",
+                    log::error!("error cargando archivo de configuración: {err}")
+                );
+                return;
             }
-        }
+        },
         None => Config::default(),
     };
 
-    let server = Server::new(config).unwrap();
-    server.start().unwrap();
+    if let Err(err) = run(config) {
+        eprint!(
+            "{}",
+            log::error!("error durante la ejecución del servidor: {err}")
+        );
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn error::Error>> {
+    let server = Server::new(config)?;
+    Ok(server.start()?)
+}
+
+fn read_config_file(path: &Path) -> Result<Config, Box<dyn error::Error>> {
+    let src = std::fs::read_to_string(path)?;
+    Ok(Config::from_config_file(&src)?)
 }
